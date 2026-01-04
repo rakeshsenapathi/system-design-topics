1b4c98f1c73fadb5e7ec805c5acd27338dd764fd
Rakesh Senapathi <rakeshsenapathi@Rakeshs-MacBook-Pro.local>
2026-01-04 16:47:39 +0530

    feat: Adds client for chunked.

--- event-driven-architecture/http-streaming/chunked/client.js
+++ event-driven-architecture/http-streaming/chunked/client.js
@@ -1,1 +1,22 @@
 // Make a fetch call and consume these chunks
+async function consumeStream() {
+    const res = await fetch('http://localhost:3003/startStream');
+
+    if(!res.ok){
+        throw new Error(`https error: ${res.status}`);
+    }
+
+    const reader = res.body.pipeThrough(new TextDecoderStream()).getReader();
+
+    while(true){
+        const { value, done} = await reader.read();
+
+        if(done) break;
+
+        console.log("chunk: " + value);
+    }
+
+    console.log("stream ended");
+};
+
+consumeStream().catch(console.error);
